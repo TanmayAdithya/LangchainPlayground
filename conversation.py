5b801c388982b3962269e7377ff070b0e60cb93d
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-10-27 21:57:49 +0530

    first commit

---
+++ conversation.py
@@ -0,0 +1,36 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+load_dotenv()
+
+chat_history = []
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+system_message = SystemMessage(content="You are a helpful AI Assistant")
+
+chat_history.append(system_message)
+
+while True:
+    query = input("You: ")
+    if query.lower() == "exit":
+        break
+    chat_history.append(HumanMessage(content=query))  # Add user message
+
+    # Get AI response using history
+    result = llm.invoke(chat_history)
+    response = result.content
+    chat_history.append(AIMessage(content=response))  # Add AI message
+
+    print(f"AI: {response}")
+
+
+print("---- Message History ----")
+print(chat_history)
